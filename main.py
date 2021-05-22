d8374816b277b8b8cf0c60a910c930c76acf249b
RussellHRowe <rcsrhr@gmail.com>
2021-05-22 18:56:29 -0400

    commit minor change

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ tkWin.title="New phthon Win"
 tkWin.geometry("600x400")
 tkLabel1=tk.Label(text="Label1")
 tkLabel1.grid(column=0,row=0)
+tkField1=tk.Text(width=40)
+tkField1.grid(column=0,row=1)
 tkWin.mainloop()
